43188c5b18e23bdc46ec043f71a44c873a1a5bec
Ahmet Yılmaz <ahmet1106yilmaz@gmail.com>
2026-01-24 16:35:33 +0300

    mail-domain-guncellendi

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,8 +1,37 @@
 import {Client as WorkflowClient} from "@upstash/workflow";
 import config from "@/lib/config";
+import { Client as QstashClient, resend } from "@upstash/qstash";
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
 
+const qstashclient = new QstashClient({ token: 
+  config.env.upstash.qstashToken 
+});
+
+export const sendEmail = async ({
+  email, 
+  subject, 
+  message
+} : {
+  email: string,
+  subject: string,
+  message: string
+}) => {
+
+  await qstashclient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Ahmet Yılmaz <ahmetyilmaz@ahmet-yilmaz-cs.com>",
+      // from "University Library <university-library@example.com>"
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+}
